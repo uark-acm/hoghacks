8cb5e2a8ec5873f0e63ce03e768d3a2b859c75c7
Alex Prosser <apsoutherngaming07@gmail.com>
2024-02-06 11:23:01 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NODE_ENV === 'production' ? '/hoghacks' : '';
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
+  basePath,
+  assetPrefix: `${basePath}/`
 };
 
 export default nextConfig;
