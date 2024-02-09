fd069ad27c4de07bccaa108d72bffcc35bffa43e
Alex Prosser <apsoutherngaming07@gmail.com>
2024-02-08 22:21:23 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
             <p className={'text-4xl text-center lg:text-left mb-[10px] pressstart'}>What is HogHacks?</p>
             <div className={'flex flex-row items-center justify-start ml-[40px] mr-[40px]'}>
                 <div className={'text-lg text-right'}>
-                    <p className={'mb-[10px]'}>HogHacks is a hack-a-thon hosted by the Association for Computing Machinery (ACM) at the University of Arkansas. It's an overnight software engineering competition that takes place every semester.</p>
+                    <p className={'mb-[10px]'}>HogHacks is a hack-a-thon hosted by the Association for Computing Machinery (ACM) at the University of Arkansas. It&apos;s an overnight software engineering competition that takes place every semester.</p>
                     <p className={'mb-[10px]'}>All students are welcome to take part regardless of school, education level, or major. All majors and backgrounds are welcome! Students have a chance to use their skills to create a collaborative project within 24 hours.</p>
                     <p className={'mb-[10px]'}>Teams compete to develop a working prototype to present in front of a panel of judges. Judges from different technical backgrounds are sourced from different companies across NWA.</p>
                     <button className={'bg-th-primary hover:bg-th-secondary p-5 text-2xl text-white rounded-lg mt-[20px] pressstart'}>Register Now!</button>
